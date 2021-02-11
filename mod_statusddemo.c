f54332757e989e3b810b2609ae44b90e7dc52e66
William Felipe Welter <wfelipew@gmail.com>
2021-02-11 01:01:12 -0300

    Register/Unregister device and set file_operations handlers

--- mod_statusddemo.c
+++ mod_statusddemo.c
@@ -8,13 +8,42 @@
 
 
 /*Set file operations handle functions */
+int statusd_open(struct inode *inode, struct file *fileptr){
+	return 1;
+}
 
+int statusd_release(struct inode *inode, struct file *fileptr){
+	return 1;
+}
 
+ssize_t statusd_read(struct file *fileptr, char *buf, size_t count, loff_t *f_pos){
+	return 1;
+}
+
+ssize_t statusd_write( struct file *fileptr, const char *buf, size_t count, loff_t *f_pos){
+	return 1;
+}
+
+struct file_operations  statusd_fops= {
+    .read = statusd_read,
+    .write = statusd_write,
+    .open = statusd_open,
+    .release = statusd_release
+};
+
+int major;
 
 static int statusd_init(void){
 	printk(KERN_ALERT "Statusd_demo started \n");
 
-	/*Create character device*/
+	/*Create character device with dynamic Major number*/
+	major = register_chrdev(0, "statusd_demo", &statusd_fops); 
+	if( major < 0 ){
+		printk(KERN_ALERT "Failed to create chracter device");
+		return -1;
+	}
+
+	/*Allocate memory*/
 	
 	return 0;
 }
@@ -22,6 +51,8 @@ static int statusd_init(void){
 
 static void statusd_exit(void){
 	printk(KERN_INFO "Statusd_demo stopped");
+	/*Unregister character device*/
+	unregister_chrdev(major,"statusd_demo");
 }
 
 
